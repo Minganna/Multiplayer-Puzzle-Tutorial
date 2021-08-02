1ea74fae4e7a603af4060057265d6c58bce62821
unknown <s4929892@bournemouth.ac.uk>
2021-08-02 16:30:56 +0100

    started pressure plate logic

---
+++ Source/PuzzlePlatforms/Actor/PlatformTrigger.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlatformTrigger.generated.h"
+
+class UBoxComponent;
+UCLASS()
+class PUZZLEPLATFORMS_API APlatformTrigger : public AActor
+{
+	GENERATED_BODY()
+public:	
+	// Sets default values for this actor's properties
+	APlatformTrigger();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		UBoxComponent* TriggerVolume;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		UStaticMeshComponent* BaseMesh = nullptr;
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
